c7355d383490596b62a9c04f8b4a53ff75a67bc1
alexandra.motulskaya.consultant@nielsen.com <alexandra.motulskaya.consultant@nielsen.com>
2021-05-10 21:31:58 +0300

    Task 6 submition

--- src/main/scala/scala2021/amotulskaya/task06/Form.scala
+++ src/main/scala/scala2021/amotulskaya/task06/Form.scala
@@ -1,29 +1,77 @@
 package scala2021.amotulskaya.task06
 
+import cats.data.{EitherT, ValidatedNec}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import cats.implicits._
+
+import scala.concurrent.Future
+
 object Form {
-  /*
-    object Sex extends Enumeration{
-    val Male, Female: String = Value
+  object Gender extends Enumeration {
+    type Gender = Value
+    val Male, Female = Value
   }
-  //import Sex._
 
-  val name:String
-  val age:Int
-  val email: String
-  //val sex: Sex
-  val height:Double
+  sealed trait DVal {
+    def err: String
+  }
 
-  def isValidName(name: String) = !name.isEmpty
+  case object NameIsInvalid extends DVal {
+    override def err: String = "Name is invalid"
+  }
 
+  case object AgeIsInvalid extends DVal {
+    override def err: String = "Age is invalid"
+  }
 
+  case object emailIsInvalid extends DVal {
+    override def err: String = "Email is invalid"
+  }
 
+  case object heightIsInvalid extends DVal {
+    override def err: String = "Height is invalid"
+  }
 
-    def isValid (sex: sex.Value) {
-    sex match {
-      case Male => println("Male")
-      case Female => println("Female")
-    }
+  case object genderIsInvalid extends DVal {
+    override def err: String = "Gender is invalid"
+  }
 
-}
-*/
+  type ValidationResult[A] = ValidatedNec[DVal, A]
+
+  def valName(name: String): ValidationResult[String] = if (name.matches("^[a-z]+$")) name.validNec else NameIsInvalid.invalidNec
+
+  def valAge(age: Int): ValidationResult[Int] = if (age > 0) age.validNec else AgeIsInvalid.invalidNec
+
+  def valEmail(email: String): ValidationResult[String] = if ( email.matches("^(.+)@(\\S+)$")) email.validNec else emailIsInvalid.invalidNec
+
+  def valHeight(height: Double): ValidationResult[Double] = if (height > 100) height.validNec else heightIsInvalid.invalidNec
+
+  import Gender._
+
+  def valHeight(height: Double, sex: Gender): ValidationResult[Double] = if (height > 100 & sex == Male ) height.validNec else heightIsInvalid.invalidNec
+
+  def valGender(sex: Gender): ValidationResult[Gender] = if (sex == Female  ||  sex == Male ) sex.validNec else genderIsInvalid.invalidNec
+
+  //case class User(name: String, age: Int)
+  case class User(name:String, age: Int, email: String, sex: Gender, height: Double)
+
+  def validateForm(name: String, age: Int, email: String, sex: Gender, height: Double): ValidationResult[User] =
+    (valName(name), valAge(age), valEmail(email), valGender(sex), valHeight(height,sex)).mapN(User)
+
+  def validateFormPar(name: String, age: Int, email: String, sex: Gender, height: Double): Future[ValidationResult[User]] = {
+    val nameF = Future(valName(name))
+    val ageF = Future(valAge(age))
+    val emailF = Future(valEmail(email))
+    val sexF = Future(valGender(sex))
+    val heightF = Future(valHeight(height))
+
+    for {
+      name <- nameF
+      age <- ageF
+      email <- emailF
+      sex <- sexF
+      height <- heightF
+    } yield (name, age, email, sex, height).mapN(User)
+  }
 }
--- src/main/scala/scala2021/amotulskaya/task06/Main.scala
+++ src/main/scala/scala2021/amotulskaya/task06/Main.scala
@@ -1,12 +1,11 @@
 package scala2021.amotulskaya.task06
 
-import cats.data.{EitherT, ValidatedNec}
-
-import scala.concurrent.Future
-import scala.util.Try
-import cats.implicits._
+import cats.data.Validated.{Invalid, Valid}
+import scala2021.amotulskaya.task06.Form.Gender._
+import scala2021.amotulskaya.task06.Form._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+
 object Main extends App {
 
   /*
@@ -24,51 +23,16 @@ Email - стандартная валидация на regexp, может быт
 
 * */
 
-  sealed trait DVal {
-    def err: String
+  println(validateForm("user", 9, "email@dot.com", Male,100))
+  println(validateForm("user", 0, "email@ com", Female,0))
+
+  val result = validateForm("user", 0, "email@ com", Female,0)
+  val temp = result match {
+    case Valid(_)        => List()
+    case Invalid(errors) => errors.map(x => x.err)//.toNonEmptyList
   }
+  println(temp)
+  //println(validateForm("user", 0, "email@ com", Female,0).toEither.map())
 
-  case object NameIsInvalid extends DVal {
-    override def err: String = "Name is invalid"
-  }
-
-  case object AgeIsInvalid extends DVal {
-    override def err: String = "Age is invalid"
-  }
-
-  type ValidationResult[A] = ValidatedNec[DVal, A]
-
-  def valName(name: String): ValidationResult[String] = if (name.matches("^[a-z]+$")) name.validNec else NameIsInvalid.invalidNec
-
-  def valAge(age: Int): ValidationResult[Int] = if (age > 0) age.validNec else AgeIsInvalid.invalidNec
-
-  case class User(name: String, age: Int)
-
-  def validateForm(name: String, age: Int): ValidationResult[User] =
-    (valName(name), valAge(age)).mapN(User)
-
-  def validateFormPar(name: String, age: Int): Future[ValidationResult[User]] = {
-    val nameF = Future(valName(name))
-    val ageF = Future(valAge(age))
-
-    for {
-      name <- nameF
-      age <- ageF
-    } yield (name, age).mapN(User)
-  }
-
-  println(validateForm("user", 9))
-  println(validateForm("user", 0))
-
-  object WeekDay extends Enumeration {
-    type WeekDay = Value
-    val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
-  }
-
-  import WeekDay._
-
-  def isWorkingDay(d: WeekDay) = !(d == Sat || d == Sun)
-
-  WeekDay.values filter isWorkingDay foreach println
-
+  println(validateFormPar("user", 0, "email@ com", Female,0).onComplete(println))
 }
